079423291fe52337a0b8249223c214be2f03ab23
Foebry <rain_fabry@hotmail.Com>
2021-03-05 15:23:13 +0100

    Allowing for variable base dir

--- Logger.py
+++ Logger.py
@@ -1,68 +1,46 @@
-import datetime
+from datetime import datetime
+import time
+import os
+
+
 class Logger:
 
     def __init__(self, loc, rot='h'):
-        self.loc = loc
+        logs = os.path.join(loc, 'Logs')
+        sql = os.path.join (logs, 'SQL')
+        debug = os.path.join(logs, 'Debug')
+        error = os.path.join(logs, 'Error')
+
+        if not os.path.exists(logs):
+            os.makedirs(logs)
+            os.makedirs(sql)
+            os.makedirs(debug)
+            os.makedirs(error)
+
+        else:
+            if not os.path.exists(sql): os.makedirs(sql)
+            if not os.path.exists(debug): os.makedirs(debug)
+            if not os.path.exists(error): os.makedirs(error)
+
         rots = {
             's': 1,
             'm': 60,
             'h': 3600,
-            'd': 86400
+            'd': 86400,
+            'w': 604800
         }
         self.rot = rots[rot]
 
-        error = open(f'{self.loc}/error{datetime.date}{datetime.hour}.log', 'a')
-        sql = open(f'{self.loc}/sql/{datetime.date}{datetime.hour}.log', 'a')
-        debug = open(f'{self.loc}/debug/{datetime.date}{datetime.hour}.log', 'a')
 
-
-        self.error = error
-        self.sql = sql
-        self.debug = debug
-
-        error.close()
-        sql.close()
-        debug.close()
-
-        self.entries = {
-            'error': datetime.now(),
-            'sql': datetime.now(),
-            'debug': datetime.now()
-        }
 
     def CreateNewFile(self, file):
-        if file == 'error':
-            error = open(f'{self.loc}/{file}{datetime.date}{datetime.hour}.log', 'a')
-            self.error = error
-            error.close()
-        elif file == 'sql':
-            sql = open(f'{self.loc}/{file}{datetime.date}{datetime.hour}.log', 'a')
-            self.sql = sql
-            sql.close()
-        elif file == 'debug':
-            debug = open(f'{self.loc}/{file}{datetime.date}{datetime.hour}.log', 'a')
-            self.debug = debug
-            debug.close()
-
-        self.enties[file] = datetime.now()
+        pass
 
     def Error(self, msg):
-        if self.entries['error'] + self.rot > datetime.now(): self.CreateNewFile('error')
-
-        self.debug.open()
-        self.debug.write(f'{datetime.now()} - {msg}')
-        self.debug.close()
+        pass
 
     def SQL(self, msg):
-        if self.entries['sql'] + self.rot > datetime.now(): self.CreateNewFile('sql')
-
-        self.sql.open()
-        self.sql.write(f'{datetime.now()} - {msg}')
-        self.sql.close()
+        pass
 
     def Debug(self, msg):
-        if self.entries['debug'] + self.rot > datetime.now(): self.CreateNewFile('debug')
-
-        self.debug.open()
-        self.debug.write(f'{datetime.now()} - {msg}')
-        self.debug.close()
+        pass
