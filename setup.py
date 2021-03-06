e1869fea77d43611660332c3e7087539be117e02
Foebry <rain_fabry@hotmail.Com>
2021-03-06 14:09:08 +0100

    created setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name = 'logger',
+    version = 1.0,
+    packages = [],
+    url = "https://github.com/Foebry/Logger",
+    licence = '',
+    author = "Foebry",
+    author_email = "rain_fabry@hotmail.com",
+    description = "Rotational logger for logging. Supports 3 levels Error, SQL, Debug"
+)
