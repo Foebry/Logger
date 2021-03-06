99c8ef2a0d51ee17a1379caeaf163075579ea2f1
Foebry <rain_fabry@hotmail.Com>
2021-03-06 11:17:23 +0100

    Finished IsValidTimeFrame method, allowing the log files to rotate around the given time interval

--- Logger.py
+++ Logger.py
@@ -4,6 +4,7 @@ import time
 import os
 
 
+
 class Logger:
 
     def __init__(self, loc, rot='h'):
@@ -38,25 +39,39 @@ class Logger:
         dir = os.path.join(self.path, file)
         if not os.path.exists(dir): os.makedirs(dir)
 
-        if self.rot == 'w': filename = f'Week {datetime.date(2021, 3, 5).strftime("%V")}.log'
-        elif self.rot == 'd': filename = f'{datetime.datetime.now().date()}.log'
-        elif self.rot == 'h': filename = f'{datetime.datetime.now().date()}_{datetime.datetime.now().hour}h.log'
-        elif self.rot == 'm': filename = f'{datetime.datetime.now().date()}_{datetime.datetime.now().hour}h_{datetime.datetime.now().minute}m.log'
-        elif self.rot == 's': filename = f'{datetime.datetime.now().date()}_{datetime.datetime.now().hour}h_{datetime.datetime.now().minute}m_{datetime.datetime.now().second}s.log'
+        now = datetime.datetime.now()
+        if self.rot == 'w': filename = f'Week {datetime.date(now.year, now.month, now.day).strftime("%V")}.log'
+        elif self.rot == 'd': filename = f'{now.date()}.log'
+        elif self.rot == 'h': filename = f'{now.date()}_{now.hour}h.log'
+        elif self.rot == 'm': filename = f'{now.date()}_{now.hour}h_{now.minute}m.log'
+        elif self.rot == 's': filename = f'{now.date()}_{now.hour}h_{now.minute}m_{now.second}s.log'
 
         self.files[file] = os.path.join(self.path, file, filename)
         temp = open(self.files[file], 'a')
         temp.close()
         self.timings[file] = datetime.datetime.now()
 
+
     def write(self, file, msg):
         self.IsValidTimeFrame(file)
         temp = open(os.path.join(self.path, file, self.files[file]), 'a')
-
         temp.write(f'{datetime.datetime.now()} - {msg} \n')
+        temp.close()
+
 
     def IsValidTimeFrame(self, file):
-
+        creation = self.timings[file]
+        now = datetime.datetime.now()
         if self.files[file] is None: self.CreateNewFile(file)
         else:
-            pass
+            diff_week = creation is None or (not datetime.date(creation.year, creation.month, creation.day).strftime("%V") == datetime.date(now.year, now.month, now.day).strftime("%V"))
+            diff_day = creation is None or (not now.date() == creation.date())
+            diff_h = creation is None or (diff_day or not now.hour == creation.hour)
+            diff_m = creation is None or (diff_h or not now.minute == creation.minute)
+            diff_s = creation is None or (diff_m or not now.second == creation.second)
+
+            if self.rot == 'w' and diff_week: self.CreateNewFile(file)
+            elif self.rot == 'd' and diff_day: self.CreateNewFile(file)
+            elif self.rot == 'h' and diff_h: self.CreateNewFile(file)
+            elif self.rot == 'm' and diff_m: self.CreateNewFile(file)
+            elif self.rot == 's' and diff_s: self.CreateNewFile(file)
